23a4a8d37586ca543b021f3511c32dd41443a80d
KillianOuzet <killianouzet@gmail.com>
2025-02-09 20:20:01 +0100

    feature: ajout d'un simulateur de match  avec la librarie axios pour les requêtes http

---
+++ apps/realtime-elo-ranker-simulator/src/simulator.ts
@@ -0,0 +1,80 @@
+import axios from 'axios';
+
+const MATCH_URL = 'http://localhost:3000/api/match';
+const PLAYER_URL = 'http://localhost:3000/api/player';
+
+interface Player {
+  id: string;
+  rank: number;
+}
+
+interface MatchResult {
+  winner: string;
+  loser: string;
+  draw?: boolean;
+}
+
+const genericPlayers: Player[] = [
+  { id: 'Player1', rank: 1200 },
+  { id: 'Player2', rank: 1100 },
+  { id: 'Player3', rank: 1300 },
+  { id: 'Player4', rank: 1250 },
+];
+
+async function getPlayers(): Promise<Player[]> {
+  try {
+    const response = await axios.get(PLAYER_URL);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching players:', error.message);
+    return genericPlayers;
+  }
+}
+
+function getRandomPlayer(players: Player[]): Player {
+  const index = Math.floor(Math.random() * players.length);
+  return players[index];
+}
+
+function simulateMatch(players: Player[]): MatchResult {
+  const player1 = getRandomPlayer(players);
+  let player2 = getRandomPlayer(players);
+
+  // Vérifier juste si le deuxième n'est pas le même joueur que le premier (pour évité de faire tout pt)
+  while (player1.id === player2.id) {
+    player2 = getRandomPlayer(players);
+  }
+
+  const winner = Math.random() > 0.5 ? player1 : player2;
+  const loser = winner === player1 ? player2 : player1;
+
+  return {
+    winner: winner.id,
+    loser: loser.id,
+    draw: false,
+  };
+}
+
+async function sendMatchResult(result: MatchResult) {
+  try {
+    const response = await axios.post(MATCH_URL, result);
+    console.log('Match result sent:', response.data);
+  } catch (error) {
+    console.error('Error sending match result:', error.message);
+  }
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function simulateMatches() {
+  const players = await getPlayers();
+  while (true) {
+    const result = simulateMatch(players);
+    await sendMatchResult(result);
+    await delay(500); // pour attendre 0.5s entre chaque match
+  }
+}
+
+simulateMatches();
